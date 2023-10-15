5d64c09ba49bc196a8c3e8ef216c417ac342562c
Lorenzo Puliti <plorenzo@disroot.org>
2023-10-15 15:27:30 +0200

    testsuite: test only a list of selected services
    Rethink how the testsuite works: the test currently fails due to
    excessive usage of resources (memory, disk space, timeout) both on
    Salsa and locally, so the testsuite is changed as follows:
    * test services from a list in d/tests/sv-testlist: this allow to
      decrease the number of services tested (for example by testing only
      recently added services or services suspected to be buggy) working
      around the resource limitation that causes the test failure
    * adapt commands and paths to the new services layout in runit 2.1.2-56
    * depends on runit >=2.1.2-56 (because of the new services layout)

--- debian/tests/services-test.sh
+++ debian/tests/services-test.sh
@@ -13,22 +13,22 @@ check_sv () {
 # stat --> run | down
 for n in 1 2 3 4 5 6 7 8 9 ; do
 	[ "$n" = '9' ] && return 1 #failed
-	if [ ! -p "/etc/sv/$service/supervise/ok" ]; then
+	if [ ! -p "/etc/service/$service/supervise/ok" ]; then
 		echo "ok pipe: not found"
 		sleep 0.7 && continue
 	fi
-	if [ ! -e "/etc/sv/$service/supervise/stat" ]; then
+	if [ ! -e "/etc/service/$service/supervise/stat" ]; then
 		echo "stat: not found"
 		sleep 0.7 && continue
 	else
-		stat=$(cat "/etc/sv/$service/supervise/stat")
+		stat=$(cat "/etc/service/$service/supervise/stat")
 		if [ "$stat" != run ]; then
 			echo "stat: is down"
 			sleep 0.7 && continue
 		else
 			echo "stat: run, 1st"
 			stat=
-			stat=$(cat "/etc/sv/$service/supervise/stat")
+			stat=$(cat "/etc/service/$service/supervise/stat")
 			sleep 0.5
 			if [ "$stat" = run ]; then # twice in a row to reduce chance of racy 'run'
 				return 0 # success!
@@ -41,6 +41,7 @@ done
 }
 
 apt-get update
+apt-get -y -t experimental install runit # needed to install 2.1.2-56 from experimental
 apt-get -y install runit-services runit-run
 nfailed=0
 
@@ -52,16 +53,16 @@ fi
 #touch /etc/runit/verbose
 
 #test rsyslog and dbus first, many other services will fail if these two fails
-if test -d /etc/sv/rsyslog ; then
+if [ -d /etc/sv/rsyslog ] || [ -d /usr/share/runit/sv.current/rsyslog ] ; then
 	systemctl stop rsyslog syslog.socket || true
 	systemctl disable rsyslog || true
 	systemctl mask rsyslog || true
-	update-service --add /etc/sv/rsyslog
+	update-service --add rsyslog
 else
-	echo "FAIL: rsyslog: no service directory for /etc/sv/rsyslog"
+	echo "FAIL: rsyslog: no service directory for rsyslog"
 	nfailed=1
 fi
-if test -d /etc/sv/dbus ; then
+if [ -d /etc/sv/dbus ] || [ -d /usr/share/runit/sv.current/dbus ]; then
 	systemctl stop dbus dbus.socket || true
 	systemctl disable dbus || true
 	systemctl mask dbus || true
@@ -73,9 +74,9 @@ if test -d /etc/sv/dbus ; then
 	echo $?
 	echo "chmod -x dbus" # override noreplace
 	chmod -x /etc/init.d/dbus
-	update-service --add /etc/sv/dbus
+	update-service --add dbus
 else
-	echo "FAIL: dbus: no service directory for /etc/sv/dbus"
+	echo "FAIL: dbus: no service directory for dbus"
 	nfailed=1
 fi
 pidof runsvdir  || true
@@ -100,6 +101,12 @@ echo 'disable *' > /etc/systemd/system-preset/99-default.preset
 # test services now
 for dir in sv/* ; do
 	service=$(basename "$dir")
+	if [ -e debian/tests/sv-testlist ]; then
+		if ! cat debian/tests/sv-testlist | grep -E "^$service( |$)" ; then
+			echo "INFO:  $service: skipping, not in testlist"
+			continue
+		fi
+	fi
 	if cat debian/tests/sv-blacklist | grep -E "^$service( |$)" ; then
 		echo "INFO: $service: skipping, blacklisted"
 		blacklisted=$(echo "$blacklisted $service")
@@ -107,29 +114,45 @@ for dir in sv/* ; do
 	fi
 	pkgname=$(apt-file search /etc/init.d/"$service" | grep -E "$service"$ | cut -d: -f1 | head -n 1)
 	if [ -z "$pkgname" ]; then
+		# systemd services are being moved to /usr as mitigation for usrmerge
 		pkgname=$(apt-file search /lib/systemd/system/"$service".service | grep -E "$service.service"$ | cut -d: -f1 | head -n 1)
+	fi
+	if [ -z "$pkgname" ]; then
+		pkgname=$(apt-file search /usr/lib/systemd/system/"$service".service | grep -E "$service.service"$ | cut -d: -f1 | head -n 1)
 	fi
 	if [ -z "$pkgname" ]; then
 		echo "WARNING: can't find a deb package for $service service"
 		echo "WARNING: can't perform test for $service, skipping .."
 		skipped=$(echo "$skipped $service")
 		continue
-		#use bin file as fallback
+		#TODO: use bin file as fallback
 	else
 		rpkglist=$(echo "$rpkglist $pkgname")
 		i=$((i + 1))
 	fi
 	apt-get -y install "$pkgname"
-	
-	if ! test -d /etc/sv/"$service" ; then
-		echo "FAIL: $service: no service directory for /etc/sv/$service"
-		nfailed=$((nfailed+1))
-		failed=$(echo "$failed $service")
-		continue
-	fi
 
 	systemctl stop "$service" # disable //mask
-	update-service --add /etc/sv/"$service" #necessary to enable under systemd
+	#necessary to enable under systemd
+	if ! update-service --add "$service" ; then
+		[ ! -d /etc/sv/"$service" ] && echo "no service found in /etc/sv/$service"
+		[ ! -d /usr/share/runit/sv.current/"$service" ] && echo "no service found in /usr/share/runit/sv.current/$service"
+		if [ -d /usr/share/runit/sv.src/"$service" ]; then
+			echo "WARNING: bug in runit trigger $service found in sv.src!"
+			CPSV_DEST=/usr/share/runit/sv.now cpsv -f s || true
+			if [ ! -d /usr/share/runit/sv.current/"$service" ]; then
+				echo "FAIL: $service: no service directory found for $service"
+				nfailed=$((nfailed+1))
+				failed=$(echo "$failed $service")
+				continue
+			fi
+		else
+			echo "FAIL: $service: no service directory found for $service"
+			nfailed=$((nfailed+1))
+			failed=$(echo "$failed $service")
+			continue
+		fi
+	fi
 	systemctl kill -s 14 runit.service # SIGALRM to force rescan
 	sleep 7
 	
@@ -151,6 +174,7 @@ for dir in sv/* ; do
 		fi
 	fi
 	sv s "$service"   # print sv status, should be up
+	update-service --remove "$service" #disable, to avoid conflicts
 
 	if [ "$i" = '5' ]; then
 		apt-get -y purge $rpkglist
